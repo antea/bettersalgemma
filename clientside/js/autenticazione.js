8f322c5e091c4aabc48c70bc3b9399e606120a2d
Matteo Salvà <salv.matteo@gmail.com>
2013-05-21 13:07:11 +0200

    Aggiunta cifratura password e inizio pagina calendar

--- clientside/js/autenticazione.js
+++ clientside/js/autenticazione.js
@@ -2,14 +2,12 @@ function AutenticazioneCtrl ($scope, $http) {
 	$scope.users=[];
 	$scope.errors=[];
 	$scope.submitLogin = function (){
-			$http.get('http://localhost:8585/login/'+$scope.userName+'/'+$scope.pw).
+			$http.get('http://localhost:8585/login/'+$scope.userName+'/'+CryptoJS.MD5($scope.pw)).
 			success(function (data, status, headers, config) {
 				$scope.errors = [];
 				$scope.users = data;
 			}).
 			error(function (data, status, headers, config) {
-				console.log(status);
-				console.log(data);
 				if (status===401) {
 					$scope.errors=[{subject:"Errore di autenticazione:", description:"Username e/o password errati"}];
 				} else{
