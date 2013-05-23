d8b66681bbae80f653b9cff71e05b98566b67b81
Matteo Salvà <salv.matteo@gmail.com>
2013-05-23 11:33:47 +0200

    Aggiunti routing pagine e logout

---
+++ clientside/js/controllers.js
@@ -0,0 +1,23 @@
+function AutenticazioneCtrl ($rootScope, $scope, $http, $location) {
+	$rootScope.users=[];
+	$scope.errors=[];
+	$scope.submitLogin = function (){
+		$http.get('http://localhost:8585/login/'+$scope.userName+'/'+CryptoJS.MD5($scope.pw)).
+		success(function (data, status, headers, config) {
+			$scope.errors = [];
+			$rootScope.users = data;
+			$location.path("/salgemmainterface");
+		}).
+		error(function (data, status, headers, config) {
+			if (status===401) {
+				$scope.errors=[{subject:"Errore di autenticazione:", description:"Username e/o password errati"}];
+			} else{
+				$scope.errors=[{subject:"Errore del server:", description:"Riprovare, se l'errore persiste contattare l'amministratore."}];
+			};
+		});
+	};
+}
+
+function CalendarCtrl ($scope, $http) {
+	$scope.storici=[];
+}
---
+++ clientside/js/salgemmainterface.js
@@ -0,0 +1,18 @@
+angular.module('salgemmaclientinterface', []).
+config(['$routeProvider', function ($routeProvider) {
+	$routeProvider.
+	when('/salgemmainterface/login', {templateUrl: 'pages/autenticazione.html', controller: AutenticazioneCtrl}).
+	when('/salgemmainterface', {templateUrl: 'pages/calendar.html', controller: CalendarCtrl}).
+	when('/', {redirectTo:'/salgemmainterface/login'}).
+	when('/salgemmainterface/logout', {redirectTo:'/salgemmainterface/login'}).
+	otherwise({templateUrl: 'pages/notFound.html'});
+}]).
+run( function ($rootScope, $location) {
+	$rootScope.$on( "$routeChangeStart", function (event, next, current) {
+		if(!$rootScope.users || $rootScope.users.length == 0){
+			if (next.templateUrl !== "pages/autenticazione.html") {
+				$location.path("/salgemmainterface/login")
+			};
+		};
+	})
+});
