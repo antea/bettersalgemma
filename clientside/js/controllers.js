57b416b0b7d4836c2c908ed77562b64ebc485a4d
Matteo Salvà <salv.matteo@gmail.com>
2013-05-28 11:36:18 +0200

    Creato array calendario in controllers.js

--- clientside/js/controllers.js
+++ clientside/js/controllers.js
@@ -1,6 +1,6 @@
 function AutenticazioneCtrl ($rootScope, $scope, $http, $location) {
-	$rootScope.users=[];
-	$scope.errors=[];
+	$rootScope.users = [];
+	$scope.errors = [];
 	$scope.submitLogin = function (){
 		$http.get('http://localhost:8585/login/'+$scope.userName+'/'+CryptoJS.MD5($scope.pw)).
 		success(function (data, status, headers, config) {
@@ -10,14 +10,47 @@ function AutenticazioneCtrl ($rootScope, $scope, $http, $location) {
 		}).
 		error(function (data, status, headers, config) {
 			if (status===401) {
-				$scope.errors=[{subject:"Errore di autenticazione:", description:"Username e/o password errati"}];
+				$scope.errors = [{subject: "Errore di autenticazione:", description: "Username e/o password errati"}];
 			} else{
-				$scope.errors=[{subject:"Errore del server:", description:"Riprovare, se l'errore persiste contattare l'amministratore."}];
+				$scope.errors = [{subject: "Errore del server:", description: "Riprovare, se l'errore persiste contattare l'amministratore."}];
 			};
 		});
 	};
 }
 
-function CalendarCtrl ($scope, $http) {
-	$scope.storici=[];
+function CalendarCtrl ($rootScope, $scope, $http) {
+	$scope.days = ['Lunedì', 'Martedì', 'Mercoledì', 'Giovedì', 'Venerdì', 'Sabato', 'Domenica'];
+	$scope.calendario = [];
+	var now = new Date();
+	var oggi = now.getDate();
+	var questoMese = now.getMonth();
+	var questoAnno = now.getFullYear();
+	var primoMeseCorrente = new Date(questoAnno, questoMese, 1).getDay()-1;
+	var ultimoMeseCorrente = new Date(questoAnno, questoMese+1, 0);
+	for (var i = 0; i < ultimoMeseCorrente.getDate()+primoMeseCorrente; i++) {
+		if (i<primoMeseCorrente) {
+			$scope.calendario[i] = {};
+		} else{
+			var dayi = new Date(questoAnno, questoMese, i+1-primoMeseCorrente);
+			$scope.calendario[i] = {numero: dayi.getDate() , 
+				giorno: $scope.days[dayi.getDay()-1], 
+				date: (dayi.getDate()) + "-" + (questoMese+1) + "-" + questoAnno
+			}
+			if (i+1-primoMeseCorrente==oggi) {
+				$scope.calendario[i].oggi = true;
+			};
+		};
+	};
+	$scope.calendario.forEach(function (arrayElement) {
+		$http.get('http://localhost:8585/storico/'+$rootScope.users[0].id+'/'+arrayElement.date).
+		success(function (data, status, headers, config) {
+			arrayElement.storico = data;
+		}).
+		error(function (data, status, headers, config) {
+			if (status!==200) {
+				$scope.errors = [{subject: "Errore del server:", description: "Riprovare, se l'errore persiste contattare l'amministratore."}];
+			}
+		});
+	});
+	console.log($scope.calendario);
 }
